3176c699a4cc335371b6eccafa20cb4202ed5f66
Han Wong <han.wong@pm.me>
2020-12-09 13:01:06 +0100

    New stuff

--- script.js
+++ script.js
@@ -1,56 +1,68 @@
-const artBeerInfo = document.querySelector('article.beer-info');
 const btnRandomBeer = document.querySelector('button.random-beer');
 const main = document.querySelector('main');
-const sectionBeers = document.querySelector('section.beers');
+const sec = document.querySelector('.beer-section');
+const sec2 = document.querySelector('.beer-section-2');
 const urlRandomBeer = 'https://api.punkapi.com/v2/beers/random';
 
 btnRandomBeer.addEventListener('click', e => getBeer(urlRandomBeer, showRandomBeer));
 
 getBeer(urlRandomBeer, showRandomBeer);
+
+main.addEventListener('click', e => console.log(e));
+
 function getBeer(url, callback) {
     fetch(url)
         .then(response => response.json())
         .then(callback)
         .catch(error => console.log(error));
 }
+
 function showRandomBeer(beer) {
-    showBeer(beer[0]);
+    renderBeer(beer[0]);
 }
 
-function showBeer(beer) {
+function renderBeer(beer) {
     const art = document.createElement('article');
-    const button = document.createElement('button');
+    const btn = document.createElement('button');
     const div = document.createElement('div');
     const div2 = document.createElement('div');
     const h4 = document.createElement('h4');
     const img = document.createElement('img');
 
-    img.addEventListener('click', e => sectionBeers.removeChild(art));
+    art.onclick = e => {
+        if (e.target != btn) sec.removeChild(art);
+        else renderBeerInfo(beer);
+    };
 
-    art.className = 'beer-info';
-    div.className = 'beer-image';
-    div2.className = 'beer-name';
-    button.className = 'see-more';
-    img.src = beer.image_url != null ? beer.image_url : './null.jpg';
+    art.className = 'beer-card';
+    div.className = 'beer-card-div';
+    div2.className = 'beer-card-div-2';
+    btn.className = 'beer-card-button';
+    img.src = beer.image_url != null ? beer.image_url : './error-404-beer-not-found-prints.jpg';
     h4.innerText = beer.name;
-    button.addEventListener('click', e => renderBeerDescription(beer));
-    button.innerText = 'See More >';
+    btn.innerText = 'See More >';
 
     div.appendChild(img);
     div2.appendChild(h4);
-    div2.appendChild(button);
+    div2.appendChild(btn);
     art.appendChild(div);
     art.appendChild(div2);
-    sectionBeers.appendChild(art);
+    sec.appendChild(art);
 }
 
-function renderBeerDescription(beer) {
-    console.log(beer);
+function renderBeerInfo(beer) {
+    btnRandomBeer.disabled = true;
+    disableButtons(document.querySelectorAll("nav>button"), true);
+    disableButtons(document.querySelectorAll(".beer-card-button"), true);
+    removeAllChildNodes(sec2);
+    sec.classList.add('transparent');
+
     const art = document.createElement('article');
-    const img = document.createElement('img');
     const div = document.createElement('div');
+    const div2 = document.createElement('div');
     const h4 = document.createElement('h4');
-    const button = document.createElement('button');
+    const img = document.createElement('img');
+    const ul = document.createElement("ul");
     const info = [
         `${beer.description}`,
         `Alcohol by volume: ${beer.abv} %`,
@@ -60,28 +72,40 @@ function renderBeerDescription(beer) {
         `Consume together with the following food: ${beer.food_pairing.join(' or ')}`,
         `Brewers tips: ${beer.brewers_tips}`,
     ];
-    const ul = document.createElement("ul");
 
-    art.className = 'beer-description';
+    art.className = 'beer-info';
+    div.className = 'beer-info-div';
+    div2.className = 'beer-info-div-2';
     ul.className = 'beer-info-list';
 
-    art.addEventListener('click', e => {
-        main.removeChild(art);
-        sectionBeers.classList.remove('invisible');
-    });
-    sectionBeers.classList.add('invisible');
+    art.onclick = e => {
+        disableButtons(document.querySelectorAll("nav>button"), false);
+        disableButtons(document.querySelectorAll(".beer-card-button"), false);
+        removeAllChildNodes(sec2);
+        sec.classList.remove('transparent');
+    };
+
     h4.textContent = beer.name;
-    img.src = beer.image_url != null ? beer.image_url : './null.jpg';
+    img.src = beer.image_url != null ? beer.image_url : './error-404-beer-not-found-prints.jpg';
+
     info.forEach((a, b) => {
         const li = document.createElement('li');
         li.textContent = a;
         ul.appendChild(li);
     });
-    div.appendChild(h4);
-    div.appendChild(ul);
-    art.appendChild(img);
+
+    div.appendChild(img);
+    div2.appendChild(h4);
+    div2.appendChild(ul);
     art.appendChild(div);
-    main.appendChild(art);
+    art.appendChild(div2);
+    sec2.appendChild(art);
+}
+
+function disableButtons(buttons, bool) {
+    for (const button of buttons) {
+        button.disabled = bool;
+    }
 }
 function removeAllChildNodes(parent) {
     while (parent.firstChild)
